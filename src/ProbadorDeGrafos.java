414256fabc02e151d3eb709efac627e840063789
TrveFenris <tinchondrius@gmail.com>
2015-11-05 11:30:49 -0300

    Create ProbadorDeGrafos.java

---
+++ src/ProbadorDeGrafos.java
@@ -0,0 +1,7 @@
+package grafos;
+
+public class ProbadorDeGrafos {
+	public static void main(String[] args) {
+		GrafoColoreado gc = new GrafoColoreado("Entradas/estrella_doble.in","Salidas/estrella_doble.out");
+	}
+}
