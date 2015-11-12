2c32582177473bc53eb4f4d1eb75611961e56c62
TrveFenris <tinchondrius@gmail.com>
2015-11-12 11:39:13 -0300

    Update MatrizSimetrica.java
    - Agregado getDimension

--- src/MatrizSimetrica.java
+++ src/MatrizSimetrica.java
@@ -6,7 +6,7 @@ public class MatrizSimetrica {
 	private boolean[] matriz;
 	private int dimension;
 	private int n;//orden de la matriz
-	private int t;//tamaño del vector
+	private int t;//tamaÃ±o del vector
 	
 	public MatrizSimetrica(int dimension){
 		this.dimension=dimension;
@@ -36,8 +36,8 @@ public class MatrizSimetrica {
 	public boolean getValor(int f, int c){
 		if(f==c)
 			return true;
-		//I = F * N + C – ( F2 + 3 * F + 2) / 2
-		//int j=f+c+(f+(f*(f-1))/2)-1; 		<--Martín
+		//I = F * N + C â€“ ( F2 + 3 * F + 2) / 2
+		//int j=f+c+(f+(f*(f-1))/2)-1; 		<--MartÃ­n
 		int i;	
 		if(f>c)
 			i=(c* n) +f -((c*c)+(3*c)+2)/2;
@@ -48,6 +48,9 @@ public class MatrizSimetrica {
 	
 	public boolean getValor(int i){
 		return matriz[i];
+	}
+	public int getDimension() {
+		return dimension;
 	}
 	
 	public void setValor(int f, int c, boolean val){
@@ -85,14 +88,14 @@ public class MatrizSimetrica {
 	}
 	
 	
-	//F = T – (Redondeo hacia arriba con cero decimales de ( 1 + ( 1 + 8 ( T- I ) )1/2  ) / 2 )
+	//F = T â€“ (Redondeo hacia arriba con cero decimales de ( 1 + ( 1 + 8 ( T- I ) )1/2  ) / 2 )
 //	public int getFila(int i){
 //		Double r=(double) (t-(Math.round((1+Math.sqrt( 1+8*(t-i)))/2)));
 //		Double k=(double)( t-(Math.round( 1 + (Math.sqrt(1+8*(t-i)) ) ) /2));
 //		return k.intValue();
 //	}
 //	
-//	//C = N – ( T – I – (  (N – F – 1)2 – (N – F – 1)  )  / 2 )
+//	//C = N â€“ ( T â€“ I â€“ (  (N â€“ F â€“ 1)2 â€“ (N â€“ F â€“ 1)  )  / 2 )
 //	public int getColumna(int i){
 //		//return n-(t-i((n-f-1)*(n-f-1)-(n-f-1)/2)));
 //		return 1;
