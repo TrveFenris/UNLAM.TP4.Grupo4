ab63f46cbcef916f8f5cfbbcdbfe009370e42d4c
TrveFenris <tinchondrius@gmail.com>
2015-11-14 14:13:03 -0300

    Cambios en el Test
    - Bloque try/catch para capturar la excepción de grafo no conexo.

--- src/grafos/Test.java
+++ src/grafos/Test.java
@@ -2,8 +2,13 @@ package grafos;
 
 public class Test {
 	public static void main(String[] args) {
-		Grafo g = GeneradorDeGrafos.generarGrafoAleatorioConPorcentajeDeAdyacencia(4, 50);
-		g.mostrarGrafo();
-		System.out.println(g.esConexo());
+		try {
+			Grafo g = GeneradorDeGrafos.generarGrafoAleatorioConPorcentajeDeAdyacencia(4, 50);
+			g.mostrarGrafo();
+			System.out.println(g.esConexo());
+		} catch (GrafoNoConexoException e) {
+			e.printStackTrace();
+		}
+		
 	}
 }
